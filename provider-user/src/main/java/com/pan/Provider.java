725eae5907028ea7820b51ed3172e3e99b0ee21a
micheal <996770483@qq.com>
2020-02-24 22:44:14 +0800

    feign

--- provider-user/src/main/java/com/pan/Provider.java
+++ provider-user/src/main/java/com/pan/Provider.java
@@ -3,12 +3,14 @@ package com.pan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * 启动类
+ * s
  */
 @SpringBootApplication
 @EnableEurekaClient // eureka 客户端
+@EnableFeignClients
 public class Provider
 {
     public static void main( String[] args )
